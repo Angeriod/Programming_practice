6d3c72e93ba8d18914b5cb70dda4ca462bc1da45
Lee JongWoo <97516571+Angeriod@users.noreply.github.com>
2023-01-13 15:46:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 900 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+N=int(input())
+number=1
+count=0
+real_num=0
+while(N!=count):
+    num_str=str(number)
+    if '666' in num_str:
+        real_num=number
+        count+=1
+    number+=1
+
+print(real_num)
