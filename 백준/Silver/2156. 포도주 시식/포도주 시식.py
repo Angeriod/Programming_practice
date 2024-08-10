30b533a799473359596fdcbf848061304e528e27
Lee JongWoo <97516571+Angeriod@users.noreply.github.com>
2024-08-11 01:59:29 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,20 +1,20 @@
-N = int(input())
+import sys
 
-grape = []
-for i in range(N):
-    grape.append(int(input()))
+N  = int(sys.stdin.readline())
+grape = [0]+[ int(sys.stdin.readline()) for _ in range(1,N+1)]
 
 dp = [0] * 10001
+dp[1] = grape[1]
 
-dp[0] = grape[0]
 if N > 1:
-    dp[1] = grape[0]+grape[1]
-
+    dp[2] = grape[1] + grape[2]
 if N > 2:
-    dp[2] = max(grape[2]+grape[1], grape[2]+grape[0], dp[1])
+    dp[3] = max(grape[2]+grape[3],grape[1]+grape[3],grape[1]+grape[2])
 
+#dp[4] = max(grape[2]+grape[3],grape[1]+grape[3]+grape[4],grape[1]+grape[2]+grape[4])
+#dp[5] = max(grape[2]+grape[3]+grape[5],dp[1]+grape[3]+grape[4],dp[1]+grape[2]+grape[4])
 
-for n in range(3,N):
+for n in range(4,N+1):
     dp[n] = max(dp[n - 2] + grape[n], dp[n - 3] + grape[n - 1]+ grape[n], dp[n - 1])
 
-print(dp[N-1])
+print(dp[N])
