210274db7f37e970d5296603bd108c91aea5ac18
Lee JongWoo <97516571+Angeriod@users.noreply.github.com>
2024-08-31 16:06:57 +0900

    [Gold V] Title: 동전 1, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,14 @@
+import sys
+
+n,k = map(int, sys.stdin.readline().split())
+coin = [int(sys.stdin.readline()) for _ in range(n)]
+
+dp = [0] *(k+1)
+dp[0] = 1
+
+
+for c in coin:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
+
+print(dp[-1])
